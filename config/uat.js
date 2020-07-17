570561b2284de44b496a709f4d347809ef61d23c
cathe-zhang <zhangle_media@hotmail.com>
2020-07-17 11:46:59 +0800

    conf: pro/uat自定义环境变量补充

--- config/uat.js
+++ config/uat.js
@@ -3,12 +3,14 @@ module.exports = {
 		APP_CONF:
 			process.env.TARO_ENV === 'h5'
 				? {
+						CUSTOMIZE_ENV: '"uat"',
 						API_HOST: '"https://xx.com/"',
 						APPID: '"wx0b32dc740be4b1f5"',
 						API_MAP_QQ: '"https://apis.map.qq.com"',
 						KEY_MAP_QQ: '"UQPBZ-RCU36-K2YS3-EMV6Y-JI6JJ-3WBUM"',
 				  }
 				: {
+						CUSTOMIZE_ENV: 'uat',
 						API_HOST: 'https://xx.com/',
 						APPID: 'wx0b32dc740be4b1f5',
 						API_MAP_QQ: 'https://apis.map.qq.com',
