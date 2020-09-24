9778958b963adbedc49294e43ab43ba1033fafde
cathe-zhang <zhangle_media@hotmail.com>
2020-09-24 17:57:53 +0800

    refactor: 删除getEnv类中的无用方法，新增userAgent和userPlatform枚举

---
+++ src/enums/userAgent.enum.ts
@@ -0,0 +1,8 @@
+enum USER_SYSTEM {
+	IOS = 'ios', // ios系统
+	ANDROID = 'android', // 安卓
+	WINDOWS_PHONE = 'windows_phone', // wp
+	UNKNOWN = 'unknown', // 未知
+}
+
+export default USER_SYSTEM
---
+++ src/enums/userPlatform.enum.ts
@@ -0,0 +1,9 @@
+enum USER_PLATFORM {
+	WEAPP = 'weapp', // 微信小程序
+	WEIXIN = 'weixin', // 微信h5环境
+	ALIPAY = 'alipay', // 支付宝小程序
+	H5 = 'h5', // h5
+	UNKNOWN = 'unknown', // 未知
+}
+
+export default USER_PLATFORM
